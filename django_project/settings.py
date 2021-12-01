618d523a10a0726e4c87a7dacd8013f843d1d773
Asker Fun <asker@vps.server.local>
2021-12-01 23:39:08 +0000

    email descomentado

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,9 +128,9 @@ CACHES = {
 if not DEBUG:
 	SECURE_SSL_REDIRECT = True
 
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = secret.EMAIL_HOST_USER
-#EMAIL_HOST_PASSWORD = secret.EMAIL_HOST_PASSWORD #past the key or password app here
-#EMAIL_PORT = 587
-#EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = secret.EMAIL_HOST_USER
+EMAIL_HOST_PASSWORD = secret.EMAIL_HOST_PASSWORD
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
