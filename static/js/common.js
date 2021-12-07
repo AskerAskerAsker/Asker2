4cba957a8479c9ac8584157c64deac8b273463a0
Asker Fun <asker@vps.server.local>
2021-12-07 09:12:22 +0000

    fix dark toggle sem cookies

--- static/js/common.js
+++ static/js/common.js
@@ -1,10 +1,10 @@
 darkbtn = document.getElementById('dark-mode');
 darkbtn.addEventListener('click', function() {
-    if (getDarkCookie() == 'false') {
-        setDarkCookie(true);
-        document.getElementById('theme-css').href = '/static/css/' + 'dark.css' + cssversion;
-    } else {
+    if (getDarkCookie() == 'true') {
         setDarkCookie(false);
         document.getElementById('theme-css').href = '/static/css/' + 'light.css' + cssversion;
+    } else {
+        setDarkCookie(true);
+        document.getElementById('theme-css').href = '/static/css/' + 'dark.css' + cssversion;
     }
 });
