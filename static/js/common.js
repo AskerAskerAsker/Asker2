218a056e4ebf3f0cb5041246850684e3b278473f
user <matheussch2@yahoo.com>
2022-01-20 00:03:16 -0300

    check_for_notifications

--- static/js/common.js
+++ static/js/common.js
@@ -8,3 +8,33 @@ darkbtn.addEventListener('click', function() {
         document.getElementById('theme-css').href = '/static/css/' + 'dark.css' + cssversion;
     }
 });
+
+async function check_for_notifications() {
+    try {
+        var notif_badge = document.getElementById('notif-badge');
+    } catch (e) {
+        return 0;
+    }
+	$.ajax({
+			url: "/update_index_check",
+			type: "get",
+			dataType: "json",
+			data: {
+				last_known_q: 1,
+			},
+			complete: function(data) {
+                new_notifications = data.responseJSON['nn'];
+                if (notif_badge) {
+                    if (new_notifications > 0) {
+                        notif_badge.innerHTML = new_notifications;
+                        notif_badge.style.display = 'block';
+                        document.title = "(" + new_notifications + ") Asker | Faça e Responda Perguntas na Comunidade!";
+                    } else {
+                        notif_badge.style.display = 'none';
+                        document.title = "Asker | Faça e Responda Perguntas na Comunidade!";
+                    }
+                }
+
+			},
+	});    
+}
